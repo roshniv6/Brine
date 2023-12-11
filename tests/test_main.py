066a016c91a9c3a27075df9ef1dc976835d47ce8
roshniv6 <33769605+roshniv6@users.noreply.github.com>
2023-12-11 22:58:13 +0530

    Created test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+import unittest
+from main import read_orders_csv, compute_monthly_revenue, compute_product_revenue, compute_customer_revenue, identify_top_customers
+
+class TestOrderAnalysis(unittest.TestCase):
+    def setUp(self):
+        self.orders = read_orders_csv("test_orders.csv")
+
+    def test_compute_monthly_revenue(self):
+        # Add test cases
+        pass
+
+    def test_compute_product_revenue(self):
+        # Add test cases
+        pass
+
+    def test_compute_customer_revenue(self):
+        # Add test cases
+        pass
+
+    def test_identify_top_customers(self):
+        # Add test cases
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
